9bacda860bcdb44d6da571b9e6b5a79ed188b79d
Silviya Yankova <silviya_yankova91@gmail.com>
2023-03-11 18:47:22 +0200

    Update task.

--- src/main/java/com/example/employeetask/view/TaskDialog.java
+++ src/main/java/com/example/employeetask/view/TaskDialog.java
@@ -35,12 +35,76 @@ public class TaskDialog {
             new MenuItem(0, "Back", () -> new Dialog(employeeService, taskService).getMainMenu())
     );
 
+    Menu updateTaskMenu = new Menu(
+            new MenuItem(1, "Title", () -> this.updateTaskTitle(() -> this.updateTaskMenu)),
+            new MenuItem(2, "Description", () -> this.updateTaskDescription(() -> this.updateTaskMenu)),
+            new MenuItem(3, "Assignee", () -> this.updateTaskAssignee(() -> this.updateTaskMenu)),
+            new MenuItem(4, "Due date", () -> this.updateTaskDueDate(() -> this.updateTaskMenu)),
+            new MenuItem(5, "Save", () -> this.saveUpdatedTask(() -> this.taskMenu)),
+            new MenuItem(0, "Cancel", () -> taskMenu)
+    );
+
     private State deleteTask(State next) {
         return null;
     }
 
+    private State updateTaskDueDate(State next) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate oldDueDate = task.getDueDate();
+        System.out.println("Update task's due date from \""
+                                   + dateTimeFormatter.format(oldDueDate) + "\" to:");
+        String input = readLn();
+        setDueDate(task, input);
+        System.out.println("Old due date: " + dateTimeFormatter.format(oldDueDate));
+        System.out.println("New due date: " + dateTimeFormatter.format(task.getDueDate()));
+        return next;
+    }
+
+    private State updateTaskAssignee(State next) {
+        System.out.println("Update task's assignee from \"" + task.getAssignee().getId() + "\" to:");
+        String newAssignee = readLn();
+        System.out.println("Old assignee id: " + task.getAssignee().getId());
+        Employee assignee = findAssignee(task, newAssignee);
+        if (assignee != null) {
+            task.setAssignee(assignee);
+            System.out.println("New assignee id: " + task.getAssignee().getId());
+        }
+        return next;
+    }
+
+    private State updateTaskDescription(State next) {
+        System.out.println("Update task's description from \"" + task.getDescription() + "\" to:");
+        String newDescription = readLn();
+        System.out.println("Old description: " + task.getDescription());
+        task.setDescription(newDescription);
+        System.out.println("New description: " + task.getDescription());
+        return next;
+    }
+
+    private State updateTaskTitle(State next) {
+        System.out.println("Update task's title from \"" + task.getTitle() + "\" to:");
+        String newTitle = readLn();
+        System.out.println("Old title: " + task.getTitle());
+        task.setTitle(newTitle);
+        System.out.println("New title: " + task.getTitle());
+        return next;
+    }
+
+    private State saveUpdatedTask(State next) {
+        taskService.updateTask(task);
+        return next;
+    }
+
     private State updateTask(State next) {
-        return null;
+        System.out.println("Enter task id you want to update:");
+        String input = readLn();
+        Task taskById = findTaskById(input);
+        if (taskById != null) {
+            System.out.println("Choose what to update. When you are finished press \"Save\"");
+            task = taskById;
+            return () -> this.updateTaskMenu;
+        }
+        return next;
     }
 
     private State readAnTask(State next) {
@@ -80,28 +144,35 @@ public class TaskDialog {
         while (task.getAssignee() == null) {
             System.out.println("Enter assignee id:");
             String inputId = readLn();
-            try {
-                int assigneeId = Integer.parseInt(inputId);
-                Optional<Employee> employee = Optional.of(employeeService.findById(assigneeId).orElseThrow());
-                task.setAssignee(employee.get());
-            } catch (NumberFormatException e) {
-                System.out.println("Please, enter a valid number.");
-            } catch (Exception e) {
-                System.out.println("Employee with id \"" + inputId + "\" does not exists.");
-            }
+            findAssignee(task, inputId);
         }
 
         while (task.getDueDate() == null) {
             System.out.println("Enter due date in format \"dd.MM.yyyy\". Example: \"20.03.2023\":");
-            setDueDate(task);
+            String input = readLn();
+            setDueDate(task, input);
         }
 
         taskService.createTask(task);
         return next;
     }
 
-    private void setDueDate(Task task) {
-        String input = readLn();
+    private Employee findAssignee(Task task, String inputId) {
+        Employee employee = null;
+        try {
+            int assigneeId = Integer.parseInt(inputId);
+            Optional<Employee> byId = Optional.of(employeeService.findById(assigneeId).orElseThrow());
+            employee = byId.get();
+            task.setAssignee(employee);
+        } catch (NumberFormatException e) {
+            System.out.println("Please, enter a valid number.");
+        } catch (Exception e) {
+            System.out.println("Employee with id \"" + inputId + "\" does not exists.");
+        }
+        return employee;
+    }
+
+    private void setDueDate(Task task, String input) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate date = LocalDate.parse(input, formatter);
         task.setDueDate(date);
