686ebb6ae942f2928e4a4bd77642d0c13d1ba157
Silviya Yankova <silviya_yankova91@gmail.com>
2023-03-11 11:47:23 +0200

    Initial commit.

---
+++ src/main/java/com/example/employeetask/EmployeeTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeTaskApplication.class, args);
+    }
+
+}
